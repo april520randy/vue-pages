8829a39b393c5ebf24040a672e4aacb4faf15a62
april520randy <april520randy@gmail.com>
2019-11-01 15:08:17 +0800

    dist

--- src/pc/views/layout.vue
+++ src/pc/views/layout.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
     <div>this is layout template</div>
-    <router-link to="/home">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    <router-view/>
+    <router-link to="/home">Home</router-link>|
+    <router-link to="/about">About</router-link>
+    <button @click="gotomobile">go to mobile</button>
+    <router-view />
   </div>
 </template>
+<script>
+export default {
+  
+  methods:{
+    gotomobile(){
+      window.location.href = '/mobile'
+    }
+  }
+}
+</script>
